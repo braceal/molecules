80e441585803ccc8c9693f996bc88e3ad125f7a7
braceal <braceal@umich.edu>
2021-02-01 19:12:27 -0500

    black style update

--- molecules/ml/callbacks/callback.py
+++ molecules/ml/callbacks/callback.py
@@ -1,5 +1,5 @@
 class Callback:
-    def __init__(self, interval=1, mpi_comm=None):
+    def __init__(self, interval: int = 1, mpi_comm=None):
         """
         Parameters
         ----------
@@ -8,7 +8,7 @@ class Callback:
         mpi_com : mpi communicator optional
         """
         if interval < 1:
-            raise ValueError('Plot interval must be int greater than 0')
+            raise ValueError("Plot interval must be int greater than 0")
 
         self.interval = interval
         self.comm = mpi_comm
@@ -16,14 +16,33 @@ class Callback:
         if (self.comm is not None) and (self.comm.Get_rank() != 0):
             self.is_eval_node = False
 
-    def on_train_begin(self, logs): pass
-    def on_train_end(self, logs): pass
-    def on_epoch_begin(self, epoch, logs): pass
-    def on_epoch_end(self, epoch, logs): pass
-    def on_batch_begin(self, batch, epoch, logs): pass
-    def on_batch_end(self, batch, epoch, logs): pass
+    def on_train_begin(self, logs):
+        pass
+
+    def on_train_end(self, logs):
+        pass
+
+    def on_epoch_begin(self, epoch, logs):
+        pass
+
+    def on_epoch_end(self, epoch, logs):
+        pass
+
+    def on_batch_begin(self, batch, epoch, logs):
+        pass
+
+    def on_batch_end(self, batch, epoch, logs):
+        pass
+
     # validation
-    def on_validation_begin(self, epoch, logs): pass
-    def on_validation_end(self, epoch, logs): pass
-    def on_validation_batch_begin(self, batch, epoch, logs, **kwargs): pass
-    def on_validation_batch_end(self, batch, epoch, logs, **kwargs): pass
+    def on_validation_begin(self, epoch, logs):
+        pass
+
+    def on_validation_end(self, epoch, logs):
+        pass
+
+    def on_validation_batch_begin(self, batch, epoch, logs, **kwargs):
+        pass
+
+    def on_validation_batch_end(self, batch, epoch, logs, **kwargs):
+        pass
