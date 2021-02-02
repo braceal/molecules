bef5d5dc36cac27f65615fde524149c0d85dbd7c
braceal <braceal@umich.edu>
2021-02-01 19:13:03 -0500

    flake8 update

--- molecules/ml/callbacks/__init__.py
+++ molecules/ml/callbacks/__init__.py
@@ -1,6 +1,6 @@
-from .loss_callback import LossCallback
-from .checkpoint_callback import CheckpointCallback
-from .save_embeddings_callback import SaveEmbeddingsCallback
-from .tsne_plot_callback import TSNEPlotCallback
-from .pointcloud_callback import PointCloud3dCallback
-from .latspace_statistics_callback import LatspaceStatisticsCallback
+from .loss_callback import LossCallback  # noqa
+from .checkpoint_callback import CheckpointCallback  # noqa
+from .save_embeddings_callback import SaveEmbeddingsCallback  # noqa
+from .tsne_plot_callback import TSNEPlotCallback  # noqa
+from .pointcloud_callback import PointCloud3dCallback  # noqa
+from .latspace_statistics_callback import LatspaceStatisticsCallback  # noqa
